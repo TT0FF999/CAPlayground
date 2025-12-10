c28f496e4bf3cd450e46e5aa43e459d9c2ea8e61
 TTOFF999  <92390838+TT0FF999@users.noreply.github.com>
2025-12-10 16:22:18 +0100

    Update route.ts

--- apps/web/app/api/drive/download/route.ts
+++ apps/web/app/api/drive/download/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getAndRefreshToken } from '../_helpers';
 
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 
 export async function POST(request: NextRequest) {
   try {
