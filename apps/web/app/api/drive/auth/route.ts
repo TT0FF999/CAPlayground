67e8069c0ecefd1af4696f0d49520926c8a439d4
 TTOFF999  <92390838+TT0FF999@users.noreply.github.com>
2025-12-10 16:20:39 +0100

    Update route.ts

--- apps/web/app/api/drive/auth/route.ts
+++ apps/web/app/api/drive/auth/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 
 export async function GET(request: NextRequest) {
   try {
