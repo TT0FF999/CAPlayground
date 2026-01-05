899bdcad30ee189e2604ef3eb4d55f847441c7e1
 TTOFF999  <92390838+TT0FF999@users.noreply.github.com>
2026-01-05 11:19:57 +0100

    Update GooeyLayer.tsx

--- apps/web/components/editor/GooeyLayer.tsx
+++ apps/web/components/editor/GooeyLayer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const GooeyLayer = () => {
   return (
     <div className="relative w-full h-full bg-black overflow-hidden flex items-center justify-center">
-      <div className="relative flex items-center justify-center w-full h-full" style={{ filter: "url('#goo-smooth')" }}>
+      <div className="relative flex items-center justify-center w-full h-full" style={{ filter: "url('#goo-apple-watch')" }}>
         <div className="absolute w-44 h-44 bg-white rounded-[38%] animate-[pulse_5s_ease-in-out_infinite]" />
         <div 
           className="absolute w-28 h-64 bg-white rounded-full animate-[spin_8s_linear_infinite]" 
@@ -25,18 +25,15 @@ export const GooeyLayer = () => {
 
       <svg className="absolute w-0 h-0 invisible">
         <defs>
-          <filter id="goo-smooth" colorInterpolationFilters="sRGB">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="15" result="blur" />
+          <filter id="goo-apple-watch" colorInterpolationFilters="sRGB">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="18" result="blur" />
             <feColorMatrix 
               in="blur" 
               mode="matrix" 
-              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -12" 
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 35 -14" 
               result="goo" 
             />
-            <feComponentTransfer in="goo">
-              <feFuncA type="discrete" tableValues="0 1" />
-            </feComponentTransfer>
-            <feGaussianBlur stdDeviation="0.5" />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
           </filter>
         </defs>
       </svg>
